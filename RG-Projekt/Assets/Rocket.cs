aabf41ef0a25d781fe798c40ac40c5b217b39545
meri-lovric <meri.lovric1@gmail.com>
2019-12-11 19:59:33 +0100

    Movement adjustments

--- RG-Projekt/Assets/Rocket.cs
+++ RG-Projekt/Assets/Rocket.cs
@@ -17,31 +17,41 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Rotate();
+        Thrust();
+
     }
-    private void ProcessInput()
+    private void Rotate()
     {
-        if (Input.GetKey(KeyCode.Space))
+        rigidBody.freezeRotation = true; // take manual control of rotation
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+        rigidBody.freezeRotation = false; // resume Physx control of rotation
+
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
         { //can thrust while rotating
             thrusting = true;
             rigidBody.AddRelativeForce(Vector3.up);
 
-            if (thrusting && !thrustSound.isPlaying)
+            if (thrusting && !thrustSound.isPlaying) // so the audio doesn't layer
             {
                 thrustSound.Play();
             }
-        
+
         }
-    else{
-        thrustSound.Stop();
-    }
-        if (Input.GetKey(KeyCode.A))
+        else
         {
-            transform.Rotate(Vector3.forward);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward);
+            thrustSound.Stop();
         }
     }
 }
