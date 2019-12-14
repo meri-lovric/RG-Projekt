a54ab6afdc4a9def227026d2b06f6dbe9f0d7bde
meri-lovric <meri.lovric1@gmail.com>
2019-12-14 13:16:53 +0100

    Launch pad changed, lighting tweaked and comet rotation implemented.

--- RG-Projekt/Assets/Oscillator.cs
+++ RG-Projekt/Assets/Oscillator.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector = new Vector3(10f,10f,10f);
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float angleX = 0f;
+    [SerializeField] float angleY = 0f;
+    [SerializeField] float angleZ = 0f;
+
     [SerializeField] float period = 2f;
     //todo remove from inspector later
-    
+
     float movementFactor; // 0 - not moved, 1 - fully moved
     Vector3 startingPos;
     // Start is called before the first frame update
@@ -19,13 +23,14 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(period <= Mathf.Epsilon) {return;}//set movement factor
-        float cycles = Time.time/period; //grows continually from 0
+        if (period <= Mathf.Epsilon) { return; }//set movement factor
+        float cycles = Time.time / period; //grows continually from 0
         const float tau = Mathf.PI * 2;
         float rawSinWave = Mathf.Sin(cycles * tau); // goes from -1 to +1
 
         movementFactor = rawSinWave / 2f + 0.5f;
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
+        gameObject.transform.Rotate(angleX, angleY, angleZ, Space.World);
     }
 }
